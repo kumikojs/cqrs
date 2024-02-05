628f246114d39a6755acd5336e03e48d1023d6e9
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-02-05 14:54:26 +0100

    test(cqrs-core): command-registry

---
+++ packages/cqrs-core/src/lib/command/internal/command-registry.test.ts
@@ -0,0 +1,70 @@
+import { CommandRegistry, CommandRegistryContract } from './command-registry';
+
+describe('CommandRegistry', () => {
+  let registry: CommandRegistryContract;
+
+  beforeEach(() => {
+    registry = new CommandRegistry();
+  });
+
+  describe('register', () => {
+    test('should register a command handler', () => {
+      // Arrange
+      const commandName = 'commandName';
+      const handler = {
+        execute: vitest.fn(),
+      };
+
+      // Act
+      const unregister = registry.register(commandName, handler);
+
+      // Assert
+      expect(registry.resolve(commandName)).toBe(handler);
+      unregister();
+      expect(() => registry.resolve(commandName)).toThrow();
+    });
+
+    test('should throw an error if command handler is already registered', () => {
+      // Arrange
+      const commandName = 'commandName';
+      const handler = {
+        execute: vitest.fn(),
+      };
+
+      // Act
+      registry.register(commandName, handler);
+
+      // Assert
+      expect(() => registry.register(commandName, handler)).toThrow();
+    });
+  });
+
+  describe('resolve', () => {
+    test('should resolve a registered command handler', () => {
+      // Arrange
+      const commandName = 'commandName';
+      const handler = {
+        execute: vitest.fn(),
+      };
+
+      // Act
+      registry.register(commandName, handler);
+
+      // Assert
+      expect(registry.resolve(commandName)).toBe(handler);
+    });
+
+    test('should throw an error if command handler is not registered', () => {
+      // Arrange
+      const commandName = 'commandName';
+
+      // Assert
+      expect(() => registry.resolve(commandName)).toThrow();
+    });
+  });
+
+  afterEach(() => {
+    vitest.clearAllMocks();
+    registry.clear();
+  });
+});
