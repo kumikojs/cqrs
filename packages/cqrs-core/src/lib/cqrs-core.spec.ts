c9f26a2b728f04e077bab1f73f9540cbca6f0eb8
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-02-05 14:23:02 +0100

    chore(cqrs-core): Use 'nx' to init package

---
+++ packages/cqrs-core/src/lib/cqrs-core.spec.ts
@@ -0,0 +1,7 @@
+import { cqrsCore } from './cqrs-core';
+
+describe('cqrsCore', () => {
+  it('should work', () => {
+    expect(cqrsCore()).toEqual('cqrs-core');
+  });
+});
---
+++ packages/cqrs-core/src/lib/cqrs-core.ts
@@ -0,0 +1,3 @@
+export function cqrsCore(): string {
+  return 'cqrs-core';
+}
