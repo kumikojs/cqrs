4ee87da1528ada269a1da2d096cc1f5aee632ddf
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-02-05 14:52:20 +0100

    feat(cqrs-core): Implement command registry

--- packages/cqrs-core/src/lib/command/internal/command-registry.ts
+++ packages/cqrs-core/src/lib/command/internal/command-registry.ts
@@ -11,4 +11,47 @@ export interface CommandRegistryContract<
   ): VoidFunction;
 
   resolve(commandName: string): CommandHandlerContract<BaseCommand>;
+
+  clear(): void;
+}
+
+export class CommandRegistry implements CommandRegistryContract {
+  protected handlers: Map<
+    CommandContract['commandName'],
+    CommandHandlerContract
+  >;
+
+  constructor() {
+    this.handlers = new Map();
+  }
+
+  public register<TCommand extends CommandContract>(
+    commandName: CommandContract['commandName'],
+    handler: CommandHandlerContract<TCommand>
+  ): VoidFunction {
+    if (this.handlers.has(commandName)) {
+      throw new Error(
+        `Command handler for "${commandName}" is already registered`
+      );
+    }
+
+    this.handlers.set(commandName, handler);
+
+    return () => this.handlers.delete(commandName);
+  }
+
+  public resolve(
+    commandName: CommandContract['commandName']
+  ): CommandHandlerContract {
+    const handler = this.handlers.get(commandName);
+    if (!handler) {
+      throw new Error(`Command handler for "${commandName}" is not registered`);
+    }
+
+    return handler;
+  }
+
+  public clear(): void {
+    this.handlers.clear();
+  }
 }
