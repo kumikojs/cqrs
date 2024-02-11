70451d7873476dc74efb980cc6318c9bfa503007
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-02-11 16:03:29 +0100

    feat(cqrs-core): Introduce redundancy management strategy for handlers

--- packages/cqrs-core/src/lib/internal/types.ts
+++ packages/cqrs-core/src/lib/internal/types.ts
@@ -1,3 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export type Nullable<T> = T | null | undefined;
 export type VoidFunction = () => void;
+export type AnyFunction = (...args: any[]) => any;
+export type PromiseAnyFunction = (...args: any[]) => Promise<any>;
---
+++ packages/cqrs-core/src/lib/strategy/internal/strategy.ts
@@ -0,0 +1,14 @@
+import type { PromiseAnyFunction } from '../../internal/types';
+
+export abstract class Strategy<TOptions> {
+  protected options: TOptions;
+
+  public constructor(options: TOptions) {
+    this.options = options;
+  }
+
+  public abstract execute<TRequest, TTask extends PromiseAnyFunction, TResult>(
+    request: TRequest,
+    task: TTask
+  ): Promise<TResult>;
+}
