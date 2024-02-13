a027245b5abb3310cd68858945a6f461a4222274
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-02-13 22:35:01 +0100

    refactor(cqrs-core): Integrate DeduplicationStrategy into TaskManager

--- packages/cqrs-core/src/lib/internal/task/task-manager.ts
+++ packages/cqrs-core/src/lib/internal/task/task-manager.ts
@@ -1,3 +1,5 @@
+import { DeduplicationStrategy } from '../../strategy/deduplication-strategy';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface TaskManagerContract<TRequest, TTask> {
   execute<TResult>(request: TRequest, task: TTask): Promise<TResult>;
@@ -8,24 +10,16 @@ export abstract class TaskManager<
   TTask extends (request: TRequest) => Promise<any>
 > implements TaskManagerContract<TRequest, TTask>
 {
-  private pendingTasks: Map<string, Promise<any>> = new Map();
+  #deduplicationStrategy: DeduplicationStrategy;
+
+  constructor() {
+    this.#deduplicationStrategy = new DeduplicationStrategy({
+      serialize: this.serialize.bind(this),
+    });
+  }
 
   async execute<TResult>(request: TRequest, task: TTask): Promise<TResult> {
-    const taskKey = this.serialize(request);
-
-    if (this.pendingTasks.has(taskKey)) {
-      return this.pendingTasks.get(taskKey);
-    }
-
-    const taskPromise = task(request);
-
-    this.pendingTasks.set(taskKey, taskPromise);
-
-    try {
-      return await taskPromise;
-    } finally {
-      this.pendingTasks.delete(taskKey);
-    }
+    return this.#deduplicationStrategy.execute(request, task);
   }
 
   protected abstract serialize(request: TRequest): string;
