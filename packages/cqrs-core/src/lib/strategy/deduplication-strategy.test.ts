028783a2ea07739c6f8aff363c68b748650b20b8
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-02-13 22:33:13 +0100

    feat(cqrs-core): Implement deduplication strategy

---
+++ packages/cqrs-core/src/lib/strategy/deduplication-strategy.test.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { DeduplicationStrategy } from './deduplication-strategy';
+
+describe('DeduplicationStrategy', () => {
+  it('should deduplicate tasks', async () => {
+    const serialize = vitest.fn((request: any) => JSON.stringify(request));
+    const task = vitest.fn((request: any) => Promise.resolve(request));
+    const task2 = vitest.fn((request: any) => Promise.resolve(request));
+    const task3 = vitest.fn((request: any) => Promise.resolve(request));
+    const strategy = new DeduplicationStrategy({ serialize });
+
+    Promise.all([
+      strategy.execute({ id: 1 }, task),
+      strategy.execute({ id: 1 }, task),
+      strategy.execute({ id: 1 }, task),
+      strategy.execute({ id: 2 }, task2),
+      strategy.execute({ id: 2 }, task2),
+      strategy.execute({ id: 3 }, task3),
+    ]);
+
+    expect(task).toHaveBeenCalledOnce();
+    expect(task2).toHaveBeenCalledOnce();
+    expect(task3).toHaveBeenCalledOnce();
+    expect(serialize).toHaveBeenCalledTimes(6);
+  });
+});
---
+++ packages/cqrs-core/src/lib/strategy/deduplication-strategy.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import type { PromiseAnyFunction } from '../internal/types';
+import { Strategy } from './internal/strategy';
+
+type DeduplicationOptions = {
+  serialize: (request: any) => string;
+};
+
+export class DeduplicationStrategy extends Strategy<DeduplicationOptions> {
+  #pendingTasks = new Map<string, Promise<any>>();
+
+  public constructor(options: DeduplicationOptions) {
+    super(options);
+  }
+
+  public async execute<TRequest, TTask extends PromiseAnyFunction, TResult>(
+    request: TRequest,
+    task: TTask
+  ): Promise<TResult> {
+    const taskKey = this.options.serialize(request);
+
+    if (this.#pendingTasks.has(taskKey)) {
+      return this.#pendingTasks.get(taskKey);
+    }
+
+    const taskPromise = task(request);
+
+    this.#pendingTasks.set(taskKey, taskPromise);
+
+    try {
+      return await taskPromise;
+    } finally {
+      this.#pendingTasks.delete(taskKey);
+    }
+  }
+}
