c4bfb361509367fc9df4494dcf4ae5c736fcf10e
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-02-28 21:55:01 +0100

    fix(cqrs-core): Remove unused event interceptor

--- packages/cqrs-core/src/lib/event/event-interceptor.ts
+++
@@ -1,5 +0,0 @@
-import type { EventContract } from './event';
-import type { Interceptor } from '../internal/interceptor/interceptor';
-
-export type EventInterceptor<TEvent extends EventContract> =
-  Interceptor<TEvent>;
