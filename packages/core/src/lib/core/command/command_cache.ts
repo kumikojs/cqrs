5bed7371844c88830209ba3563b3f9e0d219c6f8
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-09-25 00:14:07 +0200

    fix(core/command): Correctly infer 'onMutate' and simplify command type

--- packages/core/src/lib/core/command/command_cache.ts
+++ packages/core/src/lib/core/command/command_cache.ts
@@ -1,7 +1,8 @@
 import { KumikoLogger } from '../../utilities/logger/kumiko_logger';
 import { QueryCache } from '../query/query_cache';
 
-import type { QueryRegistry, QueryRequest } from '../../types/core/query';
+import type { CommandCacheContract } from '../../types/core/command';
+import type { Query, QueryRegistry } from '../../types/core/query';
 
 /**
  * Options for configuring the CommandCache.
@@ -16,7 +17,9 @@ type CommandCacheOptions = {
  *
  * @template KnownQueries - The known queries in the system, extending {@link QueryRegistry}.
  */
-export class CommandCache<KnownQueries extends QueryRegistry = QueryRegistry> {
+export class CommandCache<KnownQueries extends QueryRegistry = QueryRegistry>
+  implements CommandCacheContract<KnownQueries>
+{
   #cache: QueryCache;
   #promise?: Promise<unknown>;
   #logger: KumikoLogger;
@@ -52,31 +55,22 @@ export class CommandCache<KnownQueries extends QueryRegistry = QueryRegistry> {
    *
    * @template TQuery - The type of the query to update.
    * @template TResponse - The expected response type of the query.
-   * @param query - The query to update.
+   * @param queryOrName - The query or query name to update.
    * @param updater - The function to generate the new response based on the previous result.
    */
-  async optimisticUpdate<
-    TQuery extends
-      | KnownQueries[keyof KnownQueries]['req']
-      | KnownQueries[keyof KnownQueries]['req']['queryName'],
-    TResponse extends KnownQueries[keyof KnownQueries]['res'] = Extract<
-      KnownQueries[keyof KnownQueries],
-      {
-        query: {
-          queryName: TQuery extends QueryRequest ? TQuery['queryName'] : TQuery;
-        };
-      }
-    >['res']
-  >(
-    queryOrName: TQuery,
-    updater: (previousResult: TResponse | null | undefined) => TResponse
+
+  async optimisticUpdate<TQuery extends Query>(
+    queryOrName: TQuery['req'] | TQuery['req']['queryName'],
+    updater: (prev: TQuery['res'] | null | undefined) => TQuery['res']
   ): Promise<void> {
-    const query: QueryRequest = (queryOrName as QueryRequest).queryName
-      ? (queryOrName as QueryRequest)
-      : { queryName: queryOrName as string };
+    const query =
+      typeof queryOrName === 'string'
+        ? { queryName: queryOrName }
+        : queryOrName;
+
     this.#logger.trace('Updating query', { query });
 
-    const prevData = await this.#cache.get<TResponse>(query);
+    const prevData = await this.#cache.get<TQuery['res']>(query);
 
     const nextData = updater(prevData);
 
@@ -87,8 +81,17 @@ export class CommandCache<KnownQueries extends QueryRegistry = QueryRegistry> {
       await this.#cache.optimisticUpdate(query, prevData);
     }
 
-    this.#promise?.then(() => {
+    if (this.#promise) {
+      try {
+        await this.#promise;
+        this.#cache.invalidateQueries(query);
+      } catch (error) {
+        this.#logger.error('Promise failed, skipping cache invalidation', {
+          query,
+        });
+      }
+    } else {
       this.#cache.invalidateQueries(query);
-    });
+    }
   }
 }
