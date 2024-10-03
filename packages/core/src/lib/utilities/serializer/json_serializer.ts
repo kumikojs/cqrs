14aaa88655674fe1191b78c4da14ca1f8880448e
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-10-03 19:20:42 +0200

    feat(core): Determinastically serializer

--- packages/core/src/lib/utilities/serializer/json_serializer.ts
+++ packages/core/src/lib/utilities/serializer/json_serializer.ts
@@ -6,10 +6,12 @@ import type { Result } from '../result/result';
 export class JsonSerializer {
   serialize(data: any): Result<string, Error> {
     try {
-      const serializedData = JSON.stringify(data);
+      const serializedData = JSON.stringify(this.#sortObjectKeys(data));
       return new Success(serializedData);
     } catch (error) {
-      return new Failure(new Error('Failed to serialize data'));
+      return new Failure(
+        error instanceof Error ? error : new Error('Failed to serialize data')
+      );
     }
   }
 
@@ -18,7 +20,23 @@ export class JsonSerializer {
       const data = JSON.parse(serializedData);
       return new Success(data);
     } catch (error) {
-      return new Failure(new Error('Failed to deserialize data'));
+      return new Failure(
+        error instanceof Error ? error : new Error('Failed to deserialize data')
+      );
     }
+  }
+
+  #sortObjectKeys(data: any): any {
+    if (Array.isArray(data)) {
+      return data.map((item) => this.#sortObjectKeys(item));
+    } else if (data !== null && typeof data === 'object') {
+      return Object.keys(data)
+        .sort()
+        .reduce((sortedObj, key) => {
+          sortedObj[key] = this.#sortObjectKeys(data[key]);
+          return sortedObj;
+        }, {} as Record<string, any>);
+    }
+    return data;
   }
 }
