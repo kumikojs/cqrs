56c956ace2127926b9327bf9ad97fe1d6d5c2381
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-10-07 17:03:50 +0200

    refactor(core): Move and rename event observer into signal

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,7 +4,7 @@ export { createClient, defineFeature } from './lib/create';
 export * from './lib/core/command/command_bus';
 export * from './lib/core/command/command_subject';
 export * from './lib/core/event/event_bus';
-export * from './lib/core/event/event_observer';
+export * from './lib/core/signal/signal';
 export * from './lib/core/query/query_bus';
 export * from './lib/core/query/query_subject';
 export * from './lib/core/saga/saga';
