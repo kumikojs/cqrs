36945453df0e38e6e965722e630a7d9c538bb38e
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-10-09 17:37:42 +0200

    style(core/resilience): Remove ThrottleStrategy comments

--- packages/core/src/lib/core/resilience/strategies/throttle_strategy.ts
+++ packages/core/src/lib/core/resilience/strategies/throttle_strategy.ts
@@ -7,69 +7,22 @@ import { ThrottleException } from './exceptions/throttle_exception';
 import type { ThrottleOptions } from '../../../types/core/options/resilience_options';
 import type { AsyncFunction } from '../../../types/helpers';
 
-/**
- * A strategy designed to throttle requests, ensuring only a specific number of requests
- * are allowed within a configured time interval. If a request exceeds the configured
- * rate limit within the interval, a `ThrottleException` is thrown.
- *
- *  @example
- * ```ts
- * import { ThrottleStrategy } from '@kumiko/core';
- * import { Cache } from '@kumiko/cache';
- *
- * const cache = new Cache();
- * const strategy = new ThrottleStrategy(cache);
- *
- * let counter = 0;
- *
- * const task = async () => {
- *    counter += 1;
- *    return counter;
- * };
- *
- * const results = await Promise.all([
- *    strategy.execute({}, task),
- *    strategy.execute({}, task),
- *    strategy.execute({}, task),
- *    strategy.execute({}, task),
- *    strategy.execute({}, task),
- *    strategy.execute({}, task),
- * ]);
- *
- * console.log(results); // [1, 2, 3, 4, 5, ThrottleException]
- * ```
- */
 export class ThrottleStrategy extends Strategy<ThrottleOptions> {
   /**
    * The namespace for isolating throttle-related data in the cache.
-   * @static
    */
   static readonly namespace = 'ns_throttle';
 
-  /**
-   * Default configuration options for the throttle strategy.
-   * @private
-   * @static
-   */
   static #options: ThrottleOptions = {
     interval: '5s',
     rate: 5,
     serialize: (request) => JSON.stringify(request),
   };
 
-  /**
-   * @private The underlying cache driver responsible for storing and retrieving throttle-related data.
-   */
   #cache: Cache;
 
   #logger: KumikoLogger;
 
-  /**
-   * Creates a new instance of the ThrottleStrategy.
-   *
-   * @param cache - The cache driver used for storing throttling data.
-   * @param options - Optional configuration options to override defaults.
-   */
   constructor(
     cache: Cache,
     logger: KumikoLogger,
@@ -84,7 +37,7 @@ export class ThrottleStrategy extends Strategy<ThrottleOptions> {
 
     if (this.options.rate < 1) {
       this.#logger.error(
-        'Rate must be greater than or equal to 1. Defaulting to 5.',
+        `Throttle rate must be greater than or equal to 1. Received: ${this.options.rate}`,
         {
           topics: ['interceptors', 'resilience'],
           data: { rate: this.options.rate },
@@ -96,23 +49,7 @@ export class ThrottleStrategy extends Strategy<ThrottleOptions> {
     this.#cache = cache;
   }
 
-  /**
-   * Executes the provided task with throttling logic applied.
-   *
-   * This method checks if the same request has been made within the configured
-   * `interval`. If the request count exceeds the configured `rate` limit within that
-   * interval, a `ThrottleException` is thrown. Otherwise, the task is executed
-   * and the request count is incremented for the current interval.
-   *
-   * @template TRequest - The type of request object used for throttling.
-   * @template TTask - The type of the task to be executed. Must be an asynchronous function.
-   * @template TResult - The return type of the provided task (`TTask`).
-   * @param request - The request object used for throttling identification.
-   * @param task - The asynchronous task to be executed.
-   * @returns A promise that resolves to the result of the executed task (`TResult`).
-   * @throws {ThrottleException} Thrown when the request rate limit is exceeded within the interval.
-   */
-  public async execute<
+  async execute<
     TRequest,
     TTask extends AsyncFunction,
     TResult = ReturnType<TTask>
