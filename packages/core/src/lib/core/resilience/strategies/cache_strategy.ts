492c79b465e016eabe92eb0caf83b989373006e5
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-10-09 16:33:24 +0200

    fix(core/resilience): CacheStrategy serialization errors

--- packages/core/src/lib/core/resilience/strategies/cache_strategy.ts
+++ packages/core/src/lib/core/resilience/strategies/cache_strategy.ts
@@ -6,65 +6,26 @@ import { Strategy } from './base_strategy';
 import type { CacheOptions } from '../../../types/core/options/resilience_options';
 import type { AsyncFunction } from '../../../types/helpers';
 
-/**
- * A strategy that optimizes task execution by caching results. This strategy reduces
- * redundant computations and enhances performance by storing the results of previously
- * executed tasks for reuse.
- *
- * When a task is executed using the `CacheStrategy`, it first checks the cache for a
- * corresponding entry identified by the provided request object. If a matching entry
- * exists, the cached result is returned immediately, avoiding the need to re-execute
- * the task. If no cached entry is found, the task is executed using the provided
- * function, and the result is stored in the cache for future use.
- *
- * @example
- * ```ts
- * import { CacheStrategy, Cache } from '@kumiko/core';
- *
- * const cache = new Cache();
- * const strategy = new CacheStrategy(cache);
- *
- * async function fetchData(url: string): Promise<any> {
- *   try {
- *     const response = await strategy.execute(url, fetch);
-
- *     return await response.json();
- *   } catch (error) {
- *     console.error('Failed to fetch data:', error);
- *     // Consider throwing the error or handling it differently based on your application logic
- *   }
- * }
-
- * // Example usage
- * const someUrl = 'https://api.example.com/data';
- * fetchData(someUrl)
- *   .then(data => console.log('Fetched data:', data))
- *   .catch(error => console.error('Error fetching data:', error));
- * ```
- */
 export class CacheStrategy extends Strategy<CacheOptions> {
   static #serializer: JsonSerializer = new JsonSerializer();
 
-  /**
-   * Default configuration options for the CacheStrategy.
-   * @private
-   * @static
-   */
   static #defaultOptions: CacheOptions = {
     persist: true,
     serialize: (request) => {
       const key = CacheStrategy.#serializer.serialize(request);
 
-      if (key.isFailure()) throw new Error('Failed to serialize request data');
+      if (key.isFailure())
+        throw new Error(`Failed to serialize request: ${key.error.message}`);
+      if (key.isSuccess() && !key.value)
+        throw new Error(
+          'Failed to serialize request: Serialized data is empty'
+        );
 
       return key.value;
     },
     invalidate: false,
   };
 
-  /**
-   * @private The underlying cache driver responsible for storing and retrieving cached values.
-   */
   #cache: QueryCache;
 
   constructor(cache: QueryCache, options?: Partial<CacheOptions>) {
@@ -76,21 +37,6 @@ export class CacheStrategy extends Strategy<CacheOptions> {
     this.#cache = cache;
   }
 
-  /**
-   * Executes a task with caching optimization.
-   *
-   * Prioritizes retrieving results from the cache if available,
-   * executing the task only when necessary and storing the result for future reuse.
-   *
-   * @template TRequest - The type of request data used for the task.
-   * @template TTask - The type of the task to be executed, constrained to be an asynchronous function.
-   * @template TResult - The expected type of the result produced by the task execution.
-   *
-   * @param request - The request data to be passed to the task.
-   * @param task - The async function representing the task to be executed.
-   * @returns A promise that resolves with the result of the task execution,
-   *          either retrieved from the cache or obtained by executing the task.
-   */
   async execute<
     TRequest,
     TTask extends AsyncFunction,
